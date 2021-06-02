c340965eeee95572f22975987db40613bc3e91c8
yjyin19 <42340525+yjyin19@users.noreply.github.com>
2021-06-02 02:43:51 +0200

    test

--- src/transformers/pipelines/question_answering.py
+++ src/transformers/pipelines/question_answering.py
@@ -331,6 +331,7 @@ class QuestionAnsweringPipeline(Pipeline):
                 # Make sure non-context indexes in the tensor cannot contribute to the softmax
                 start_l = np.where(undesired_tokens_mask, -10000.0, start_l)
                 end_l = np.where(undesired_tokens_mask, -10000.0, end_l)
+                print("start_l": start_l)
 
                 # Normalize logits and spans to retrieve the answer
                 start_ = np.exp(start_l - np.log(np.sum(np.exp(start_l), axis=-1, keepdims=True)))
